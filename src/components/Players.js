bc56107b10dabf6021d4d5fc06bb3cad0f8f6497
Ville Tuhkanen <Ville Tuhkanen>
2020-12-02 17:05:20 +0200

    Sort players list with name sorter
    Players are now also sorted alphabetically.

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import { nameSorter } from '../utils/sorters';
+
 const Players = ({ players, deletePlayer }) => {
 
   return (
     <div>
       {
-        players && players
+        players && players.sort(nameSorter)
           .map(player =>
             <div key={player.id}>
               <div>{player.name}<button onClick={() => deletePlayer(player)}>Delete</button></div>
